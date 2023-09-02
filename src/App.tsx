c687d4f3ef26211b274947a759107fad634949b2
Valmir Aguiar <wefit.valmir@voeazul.com.br>
2023-09-02 12:21:22 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ function App() {
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
+          target="_blank" 
           rel="noopener noreferrer"
         >
-          Learn React
+          Teste
         </a>
       </header>
     </div>

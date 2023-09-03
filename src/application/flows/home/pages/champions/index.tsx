bce59d35f446fd38d0f6d8dcc7db3c1e7a283d36
Valmir Aguiar <wefit.valmir@voeazul.com.br>
2023-09-03 14:36:41 -0300

    feat: add and config axios

--- src/application/flows/home/pages/champions/index.tsx
+++ src/application/flows/home/pages/champions/index.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ChampionsLayout from "./layout";
 
 const Champions : React.FC = () => {
+  useEffect(() => {
+    fetch('http://ddragon.leagueoflegends.com/cdn/13.17.1/data/pt_BR/champion.json');
+    
+  }, []);
+
   return (
     <ChampionsLayout />
   );

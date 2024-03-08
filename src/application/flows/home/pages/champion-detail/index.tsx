6c81b6dbf170e188fadd2e9bc5ae0dc3de711726
Valmir Aguiar <114522580+valmir-aguiar@users.noreply.github.com>
2024-03-08 14:16:01 -0300

    fix: removing unused files

--- src/application/flows/home/pages/champion-detail/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ChampionDetailLayout from './layout';
-
-const ChampionDetail: React.FC = () => {
-  return <ChampionDetailLayout />;
-};
-
-export default ChampionDetail;

fdd6ad3ded6f4f48f42a02ae183b9391e70492b1
Valmir Aguiar <114522580+valmir-aguiar@users.noreply.github.com>
2024-05-02 18:57:24 -0300

    fix: fix base url and patch
    - change current patch
    - https added

--- src/infra/services/constants/constants.ts
+++ src/infra/services/constants/constants.ts
@@ -1,7 +1,7 @@
-const CURRENT_PATCH = '13.18.1';
-const DEFAULT_LIST_ENDPOINT: string = `http://ddragon.leagueoflegends.com/cdn/${CURRENT_PATCH}/data/pt_BR`;
-const DEFAULT_IMAGE_ENDPOINT: string = `http://ddragon.leagueoflegends.com/cdn/img/champion/loading`;
-const DEFAULT_SPLASH_ENDPOINT: string = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash`;
+const CURRENT_PATCH = '14.9.1';
+const DEFAULT_LIST_ENDPOINT: string = `https://ddragon.leagueoflegends.com/cdn/${CURRENT_PATCH}/data/pt_BR`;
+const DEFAULT_IMAGE_ENDPOINT: string = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading`;
+const DEFAULT_SPLASH_ENDPOINT: string = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash`;
 
 export {
   CURRENT_PATCH,
